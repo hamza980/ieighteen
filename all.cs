f22180c66b4ecc697d2bb8850ef96fbb6b8edbba
hamza980 <hsarwar980@gmail.com>
2020-02-29 16:13:09 +0500

    add new file in the experiment branch named all

---
+++ all.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace loginapp.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Comment_Id { get; set; }
+        public string Description { get; set; }
+       
+        //[ForeignKey("User")]
+        public int User_Id { get; set; }
+        public virtual User User { get; set; }
+
+        public int post_Id { get; set; }
+        public virtual Post post { get; set; }
+
+    }
+
+
+gvgcghchggggggggggggggggggggggggnchgcccg
+
+}
